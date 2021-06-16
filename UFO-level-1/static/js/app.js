23a2de200a8188a6b2cece889097d2c720e727c3
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-15 22:30:52 -0400

    Initial load of data for app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,44 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
 
 // YOUR CODE HERE!
+// This activity was designed to run multiple times. For each time uncomment once step, run the JavaScript code,
+// comment the step again and move to the next step. 
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+  //From Unit 14.3 Activity 03
+// // Step 5: Use d3 to update each cell's text with
+// // weather report values (weekday, date, high, low)
+// data.forEach(function(weatherReport) {
+//   console.log(weatherReport);
+//   var row = tbody.append("tr");
+//   Object.entries(weatherReport).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     // in the weather report object
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
+
+// BONUS: Refactor to use Arrow Functions!
+// data.forEach((weatherReport) => {
+//   var row = tbody.append("tr");
+//   Object.entries(weatherReport).forEach(([key, value]) => {
+//     var cell = row.append("td");
+//     cell.text(value);
+//   });
+// });
