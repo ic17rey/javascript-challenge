464ef862b505062ed8da338cf33f6420c422bba6
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-06-16 23:58:15 -0400

    Filter for date input and log resulting array

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,16 @@
 // from data.js
 //var tableData = data;
 
-// YOUR CODE HERE!
-// This activity was designed to run multiple times. For each time uncomment once step, run the JavaScript code,
-// comment the step again and move to the next step. 
-
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Get a reference to the input element on the page, form-control
+var form = d3.select(".filters");
+
+// Get a reference to the button that can be clicked to filter for a date
+var filterButton = d3.select("#filter-btn");
+
+// Console.log the sightings data from data.js
 console.log(data);
 
 data.forEach((ufoSighting) => {
@@ -18,6 +20,46 @@ data.forEach((ufoSighting) => {
       cell.text(value);
     });
   });
+
+
+// Create the event handlers, for clicking a button or hitting enter
+filterButton.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault()
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Create a variable holding the filtered data, filtered for datetime
+  var filteredData = data.filter(tbody => tbody.datetime === inputValue);
+
+  console.log(filteredData);
+  
+  filteredData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+  // if datetime = the input ofr dateInput on the form-control
+  //console.log("The filter button was clicked!");
+  // return tbody.datetime.filter(dateInput)
+
+// Looking for where datetime = the input from dateInput on the form, then want to return and display the rows
+
+
+// filterButton.on("click", handleClick);
+
+
 
   //From Unit 14.3 Activity 03
 // // Step 5: Use d3 to update each cell's text with
